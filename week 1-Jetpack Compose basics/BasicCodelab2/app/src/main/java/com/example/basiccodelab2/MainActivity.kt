23b8188f4cb47981ad96bfde149b3e8d2dc46a90
eunjilee <tkl0458@gmail.com>
2021-11-14 20:18:32 +0900

    [week1] 6. Creating columns and rows

--- week 1-Jetpack Compose basics/BasicCodelab2/app/src/main/java/com/example/basiccodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelab2/app/src/main/java/com/example/basiccodelab2/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.basiccodelab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,23 +29,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+
+    /**
+     * Composable functions can be used like any other function in Kotlin.
+     * This makes building UIs really powerful
+     * since you can add statements to influence how the UI will be displayed.
+     */
+    Column(modifier = Modifier.padding(vertical = 4.dp)){
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp)) // Text() is a composable function
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {           // children in side of a Column will be placed horizontally
+            /**
+             * omposable 시작시 weight를 주어 (alignEnd modifier 없음, fillMaxWidth와 중복)
+             * - 사용 가능한 모든 공간을 채우고 flexible 하게 만든들고
+             * - weight가 없는 다른 inflexible elements 를 밀어냄
+             */
+            Column(modifier = Modifier.weight(1f)) { // vertically
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
+        }
+
     }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicCodelab2Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
