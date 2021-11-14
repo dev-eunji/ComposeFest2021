2ebbb2b791b17fd2c2597d1db60eac0a431781a9
eunjilee <tkl0458@gmail.com>
2021-11-14 20:00:02 +0900

    [week1] 1~4 (@Composable, Surface, Modifier)

--- week 1-Jetpack Compose basics/BasicCodelab2/app/src/main/java/com/example/basiccodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicCodelab2/app/src/main/java/com/example/basiccodelab2/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.basiccodelab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiccodelab2.ui.theme.BasicCodelab2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicCodelab2Theme {
+            BasicCodelab2Theme { // depends on how your project is named.
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -24,12 +27,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * composable function is a regular function annotated with @Composable
+ * This function will produce a piece of UI
+ */
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    /**
+     * Surface()
+     * - set a different background color with 
+     * - Material components chooses an appropriate color for text.
+     *   : 흰 배경에서는 검정색 글씨가 Surcface color를 보라색으로 지정했더니 글자색이 하얀색으로 자동 변경됨
+     */
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp)) // Text() is a composable function
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Text preview")
 @Composable
 fun DefaultPreview() {
     BasicCodelab2Theme {
--- week 1-Jetpack Compose basics/README.md
+++ week 1-Jetpack Compose basics/README.md
@@ -10,3 +10,6 @@ Basics에 올려둔 코드는 테마 관련 코드 확인용입니다.
 ## 작업 후
 직접 생성한 프로젝트를 여기에 붙여 넣어주세요.
 Git 명령어 또는 [SourceTree](https://www.sourcetreeapp.com/), [GitKraken](https://www.gitkraken.com/) 등을 이용해 작업 결과를 push 해주세요.
+
+## Codelab url
+- [jetpack-compose-basics](https://developer.android.com/codelabs/jetpack-compose-basics)
